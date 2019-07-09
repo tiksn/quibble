fd0bf84926dd5d55fd948224aee816e965d078ed
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-07-09 14:18:05 +0300

    logging added

--- quibble/Program.cs
+++ quibble/Program.cs
@@ -1,5 +1,9 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace TIKSN.quibble
 {
@@ -7,10 +11,25 @@ namespace TIKSN.quibble
     {
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureServices((hostContext, services) =>
                 {
+                    services.AddAutofac();
                     services.AddHostedService<Worker>();
-                });
+                })
+                .ConfigureContainer<ContainerBuilder>(builder =>
+                {
+                    // registering services in the Autofac ContainerBuilder
+                })
+                .ConfigureLogging(builder =>
+                {
+                    builder.ClearProviders();
+                    var logger = new LoggerConfiguration()
+                        .WriteTo.ColoredConsole()
+                        .CreateLogger();
+                    builder.AddSerilog(logger);
+                })
+                .UseConsoleLifetime();
 
         public static void Main(string[] args)
         {
