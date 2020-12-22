8c728072ae90bf1c18f387f572dfb8b56e362881
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-12-22 14:58:03 +0200

    Update quibble.ps1

--- quibble.ps1
+++ quibble.ps1
@@ -4,12 +4,19 @@ param (
 
 try {
     Import-Module -Name Microsoft.Graph
+    Import-Module -Name Habitica
 
     Connect-Graph -Scopes @("Tasks.Read", "Tasks.ReadWrite")
     $mgUser = Get-MgUser
-
     Write-Host "Microsoft Graph user is $($mgUser.DisplayName)"
-    Get-MgUserTodoList -UserId $mgUser.Id
+
+    $habiticaCredentialsFilePath = Join-Path -Path $HOME -ChildPath "HabiticaCredentials"
+    Connect-Habitica -Path $habiticaCredentialsFilePath
+
+    $hUser = Get-HabiticaUser
+    Write-Host "Habitica user is $($hUser.profile.name)"
+
+    Get-MgUserTodoList -UserId $mgUser.Id -All
 }
 catch {
     throw $_
