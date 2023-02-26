b7c51df5ff8c0b85c004596c20f6b45ec0d6577e
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-02-26 20:19:38 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -2,3 +2,7 @@ Install-Module -Name Microsoft.Graph -Force -AcceptLicense
 Install-Module -Name Habitica -Force -AcceptLicense
 Install-Module -Name Pester -Force -AcceptLicense
 Install-Module -Name PSModuleDevelopment -Force -AcceptLicense
+Install-Module -Name Microsoft.PowerShell.SecretManagement -Force -AcceptLicense
+Install-Module -Name Microsoft.PowerShell.SecretStore -Force -AcceptLicense
+
+Register-SecretVault -Name SecretStore -ModuleName Microsoft.PowerShell.SecretStore -DefaultVault
