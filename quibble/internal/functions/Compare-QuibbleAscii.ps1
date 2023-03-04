3652f5472865b8927bb8aba9ce1d22dd0a53da31
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-03-04 22:59:06 +0100

    Create Compare-QuibbleAscii.ps1

---
+++ quibble/internal/functions/Compare-QuibbleAscii.ps1
@@ -0,0 +1,49 @@
+﻿function Compare-QuibbleAscii {
+<#
+    .SYNOPSIS
+        Compare Strings as ASCII
+
+    .DESCRIPTION
+        Compare Strings as ASCII
+
+    .PARAMETER Reference
+        Reference String
+
+    .PARAMETER Difference
+        Difference String
+
+    .NOTES
+
+    .LINK
+        https://github.com/tiksn/quibble
+
+    .EXAMPLE
+        Compare-QuibbleAscii -Reference 'ABC' -Difference 'DEF'
+
+        Compare Strings as ASCII
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Reference,
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Difference
+    )
+
+    begin {
+
+    }
+
+    process {
+        $targetReference = ConvertTo-QuibbleAscii -Source $Reference
+        $targetDifference = ConvertTo-QuibbleAscii -Source $Difference
+
+        $targetReference -eq $targetDifference
+    }
+
+    end {
+
+    }
+}
